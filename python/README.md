e556a13a63134164fb994e5b98b86ee7004c1849
Yoann Benoit <ybenoit@xebia.fr>
2017-03-14 23:01:11 +0100

    Update inference optimization - Remove hard coded paths

--- python/README.md
+++ python/README.md
@@ -6,3 +6,16 @@ Train and optimize a TensorFlow model for image recognition in smartphone app.
 ```
 python magritte_training/training/transfer_learning.py --image_dir="your_image_path" --how_many_training_steps=3000 --learning_rate=0.001
 ```
+
+### Optimize the model
+##### Optimize for inference
+This step will remove all unused operations in the graph
+```
+python magritte_training/optimize/optimize_for_inference.py --input_model_name="input_model_name.pb" --quantized_model_name="quantized_model_name.pb"
+```
+
+##### Quantize the weights
+Quantize the float weights to eight-bits to shrink the model size
+```
+python magritte_training/optimize/quantize.py --input="your_input_model.pb" --output_node_names="final_result" --output="your_output_model.pb" --mode=eightbit
+```
--- python/magritte_training/optimize/optimize_for_inference.py
+++ python/magritte_training/optimize/optimize_for_inference.py
@@ -8,14 +8,9 @@ FLAGS = None
 
 
 def main(_):
-    # Freeze the graph
-
-    output_frozen_graph_name = '/Users/Yoann/Documents/Xebia/tests/datamobile/model/magritte_model.pb'
-    output_optimized_graph_name = '/Users/Yoann/Documents/Xebia/tests/datamobile/model/optimized_magritte_model.pb'
-
     input_graph_def = tf.GraphDef()
 
-    with tf.gfile.Open(output_frozen_graph_name, "r") as f:
+    with tf.gfile.Open(FLAGS.input_model_name, "r") as f:
         data = f.read()
         input_graph_def.ParseFromString(data)
 
@@ -26,12 +21,24 @@ def main(_):
             tf.float32.as_datatype_enum)
 
     # Save the optimized graph
-    f = tf.gfile.FastGFile(output_optimized_graph_name, "w")
+    f = tf.gfile.FastGFile(FLAGS.quantized_model_name, "w")
     f.write(output_graph_def.SerializeToString())
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--input_model_name',
+        type=str,
+        default='/tmp/magritte/magritte_model.pb',
+        help='Path to the model.'
+    )
+    parser.add_argument(
+        '--quantized_model_name',
+        type=str,
+        default='/tmp/magritte/magritte_model_quantized.pb',
+        help='Name of the model to load.'
+    )
 
     FLAGS, unparsed = parser.parse_known_args()
     tf.app.run(main=main, argv=[sys.argv[0]] + unparsed)
