a3429e6678691090538aa64b300bc8aac83b70e9
Qian Jin <jinqian.hz@gmail.com>
2017-03-27 22:17:28 +0200

    [android] manage runtime permission

--- android/app/src/main/java/fr/xebia/magritte/CameraActivity.java
+++ android/app/src/main/java/fr/xebia/magritte/CameraActivity.java
@@ -16,32 +16,23 @@
 
 package fr.xebia.magritte;
 
-import android.Manifest;
 import android.app.Activity;
 import android.app.Fragment;
-import android.content.pm.PackageManager;
 import android.media.Image.Plane;
 import android.media.ImageReader.OnImageAvailableListener;
-import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.HandlerThread;
 import android.util.Size;
 import android.view.KeyEvent;
 import android.view.WindowManager;
-import android.widget.Toast;
-
-import java.nio.ByteBuffer;
 
 import org.tensorflow.demo.env.Logger;
 
+import java.nio.ByteBuffer;
+
 public abstract class CameraActivity extends Activity implements OnImageAvailableListener {
     private static final Logger LOGGER = new Logger();
-
-    private static final int PERMISSIONS_REQUEST = 1;
-
-    private static final String PERMISSION_CAMERA = Manifest.permission.CAMERA;
-    private static final String PERMISSION_STORAGE = Manifest.permission.WRITE_EXTERNAL_STORAGE;
 
     private boolean debug = false;
 
@@ -52,15 +43,9 @@ public abstract class CameraActivity extends Activity implements OnImageAvailabl
     protected void onCreate(final Bundle savedInstanceState) {
         LOGGER.d("onCreate " + this);
         super.onCreate(null);
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
         setContentView(R.layout.activity_camera);
-
-        if (hasPermission()) {
-            setFragment();
-        } else {
-            requestPermission();
-        }
+        setFragment();
     }
 
     @Override
@@ -115,39 +100,6 @@ public abstract class CameraActivity extends Activity implements OnImageAvailabl
     protected synchronized void runInBackground(final Runnable r) {
         if (handler != null) {
             handler.post(r);
-        }
-    }
-
-    @Override
-    public void onRequestPermissionsResult(
-            final int requestCode, final String[] permissions, final int[] grantResults) {
-        switch (requestCode) {
-            case PERMISSIONS_REQUEST: {
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED
-                        && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
-                    setFragment();
-                } else {
-                    requestPermission();
-                }
-            }
-        }
-    }
-
-    private boolean hasPermission() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            return checkSelfPermission(PERMISSION_CAMERA) == PackageManager.PERMISSION_GRANTED && checkSelfPermission(PERMISSION_STORAGE) == PackageManager.PERMISSION_GRANTED;
-        } else {
-            return true;
-        }
-    }
-
-    private void requestPermission() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            if (shouldShowRequestPermissionRationale(PERMISSION_CAMERA) || shouldShowRequestPermissionRationale(PERMISSION_STORAGE)) {
-                Toast.makeText(CameraActivity.this, "Camera AND storage permission are required for this demo", Toast.LENGTH_LONG).show();
-            }
-            requestPermissions(new String[]{PERMISSION_CAMERA, PERMISSION_STORAGE}, PERMISSIONS_REQUEST);
         }
     }
 
--- android/app/src/main/java/fr/xebia/magritte/LevelActivity.java
+++ android/app/src/main/java/fr/xebia/magritte/LevelActivity.java
@@ -1,12 +1,17 @@
 package fr.xebia.magritte;
 
+import android.Manifest;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import permissions.dispatcher.NeedsPermission;
+import permissions.dispatcher.RuntimePermissions;
 
+@RuntimePermissions
 public class LevelActivity extends AppCompatActivity {
 
     @Override
@@ -18,6 +23,17 @@ public class LevelActivity extends AppCompatActivity {
 
     @OnClick(R.id.level_one)
     public void onLevelOneClick() {
+        LevelActivityPermissionsDispatcher.startClassifierWithCheck(this);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        LevelActivityPermissionsDispatcher.onRequestPermissionsResult(this, requestCode, grantResults);
+    }
+
+    @NeedsPermission({Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE})
+    void startClassifier() {
         startActivity(new Intent(this, ClassifierActivity.class));
     }
 }
