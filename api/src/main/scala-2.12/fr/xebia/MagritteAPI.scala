bb05e3cea6cd6cbda66eb5af7f4f965d2674a48f
Sylvain Lequeux <slequeux@xebia.fr>
2017-09-24 11:40:46 +0200

    Use Env vars for AWS Keys

--- api/src/main/scala-2.12/fr/xebia/MagritteAPI.scala
+++ api/src/main/scala-2.12/fr/xebia/MagritteAPI.scala
@@ -15,24 +15,23 @@ object MagritteAPI extends App {
   private val port = 8080
   private val listen = "0.0.0.0"
 
-  implicit val system = ActorSystem()
-  implicit val materializer = ActorMaterializer()
+  implicit val system: ActorSystem = ActorSystem()
+  implicit val materializer: ActorMaterializer = ActorMaterializer()
+
+  Logger(getClass.getName).info("Loading S3 keys from environment variables")
+  val accessKey = sys.env("S3_ACCESS_KEY")
+  val secretKey = sys.env("S3_SECRET_KEY")
 
   val s3Client = AmazonS3ClientBuilder.standard()
-    .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(
-      ???,
-      ???)
-    ))
+    .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(accessKey, secretKey)))
     .withRegion(Regions.EU_WEST_1)
     .build()
 
-  implicit val bucketName = "xebia-magritte"
+  implicit val bucketName: String = "xebia-magritte"
   implicit val s3Model: S3Model = new S3Model(s3Client)
 
   val versionsRoutes = new VersionRoutes()
-  val routes = versionsRoutes.baseRoute
-  Http().bindAndHandle(routes, listen, port)
+  Http().bindAndHandle(versionsRoutes.baseRoute, listen, port)
 
   Logger(getClass.getName).info(s"Listening on $listen:$port")
-  Logger(getClass.getName).info(s"MODELS_PATH : ${versionsRoutes}")
 }
