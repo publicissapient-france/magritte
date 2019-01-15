531c59756246957f92422e44ddff2067fa8298a9
Qian Jin <qian.jin.dev@gmail.com>
2019-01-15 14:34:24 +0100

    [android] init new project

---
+++ android-v2/app/src/main/java/fr/xebia/magritte/MainActivity.kt
@@ -0,0 +1,12 @@
+package fr.xebia.magritte
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
